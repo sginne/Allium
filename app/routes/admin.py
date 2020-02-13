143dfcc37b376d57615f7719ad499ce4fe912d8a
sginne <sigin@norsula.com>
2020-02-13 13:28:11 +0200

    blueprint prototype, missing is the reason for prev.project bulkiness

--- app/routes/admin.py
+++ app/routes/admin.py
@@ -1,6 +1,26 @@
-from flask import Blueprint, render_template
-s="s"
-admin_blueprint=Blueprint('admin',__name__)
+from flask import Blueprint, render_template, session, request, make_response
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField
+from flask import current_app as app
+
+admin_blueprint = Blueprint('admin', __name__)
+
+
+# blueprint  for admin console
 @admin_blueprint.route('/admin')
-def hello_world():
-    return render_template('admin.html')
+def admin():
+    data_to_template=[]
+    # todo rework adming,may be add session, but not necessary
+    if request.cookies.get('masterkey') is not None:
+        config_to_template=app.config
+        return render_template('admin.html',config=app.config)
+    else:
+        # fixme add sessions for security
+        class AdminForm(FlaskForm):
+            master_key=PasswordField('Master key')
+            submit = SubmitField('Log in')
+        validation_form=AdminForm()
+
+        response = make_response(redirect('/admin'))
+        response.set_cookie('masterkey', '1')
+        return response
