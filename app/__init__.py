55954c7c086df66c25fce52c6bd9a8122771e957
sginne <sigin@norsula.com>
2020-02-13 13:30:45 +0200

    commenting and nonworking atm session #fixme

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,24 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,session
 from flask_sqlalchemy import SQLAlchemy
+from flask_session import Session
 
 
-db=SQLAlchemy()
-
-from . import models
-from . import routes
+db=SQLAlchemy() #db by SQLAlchemy
 
 
+from . import models #database models
+from . import routes #blueprints
 
-app = Flask(__name__, static_folder='./static/')
-app.config.from_pyfile('../allium.cfg')
-db.init_app(app)
 
-app.register_blueprint(routes.admin.admin_blueprint)
+
+app = Flask(__name__, static_folder='./static/') #here flask is born
+
+app.config.from_pyfile('../allium.cfg') #parse config into flask
+
+db.init_app(app) #db for app
+Session(app) #Session
+
+app.register_blueprint(routes.admin.admin_blueprint) #registering blueprints for admin
 @app.route('/')
 def hello_world():
     db.create_all()
