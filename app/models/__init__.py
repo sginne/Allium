a2747a13d0fb644886c026705f74b199664a0e18
sginne <sigin@norsula.com>
2020-02-14 23:36:59 +0200

    imported app, added all_tables list property includes all tables for database

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,6 @@
 from app import db
+import app
+all_tables=["goods"]
 #goods
 class Goods(db.Model):
     """
@@ -6,3 +8,5 @@ class Goods(db.Model):
     """
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64))
+    #picture = app.image_attachment('Image')
+    __tablename__="goods"
