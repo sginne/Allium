6a5f15cf4615f4402547898f3a629039891b4d1c
sginne <sigin@norsula.com>
2020-02-14 17:26:46 +0200

    removed config_from_template, app.config is sufficient

--- app/routes/admin.py
+++ app/routes/admin.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, session, request, make_response
+from flask import Blueprint, render_template, session, request, make_response,abort
 from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField
 from flask import current_app as app
@@ -9,11 +9,9 @@ admin_blueprint = Blueprint('admin', __name__)
 # blueprint  for admin console
 @admin_blueprint.route('/admin')
 def admin():
-    data_to_template=[]
-    # todo rework adming,may be add session, but not necessary
+    # todo rework adming,may be add session, but not necessary atm
     if request.cookies.get('masterkey') is not None:
-        config_to_template=app.config
-        return render_template('admin.html',config=app.config)
+        return render_template(app.config['TEMPLATE_NAME']+'/admin.html',config=app.config)
     else:
         # fixme add sessions for security
         class AdminForm(FlaskForm):
@@ -24,3 +22,6 @@ def admin():
         response = make_response(redirect('/admin'))
         response.set_cookie('masterkey', '1')
         return response
+@admin_blueprint.route('/admin_add')
+def add_item():
+    abort(400)
