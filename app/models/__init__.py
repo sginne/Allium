074392a483a4824e6ee5d9a471e9c85c56555804
sginne <sigin@norsula.com>
2020-02-14 21:19:43 +0200

    fixed comments

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,8 @@
 from app import db
 #goods
 class Goods(db.Model):
+    """
+    Class of goods to trade
+    """
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64))
