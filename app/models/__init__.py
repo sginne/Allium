92f66dc85ca2911323482d28b36edca249e196ca
sginne <sigin@norsula.com>
2020-02-15 00:02:00 +0200

    picture table added

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,12 +1,17 @@
 from app import db
 import app
-all_tables=["goods"]
+all_tables=["goods","good_picture"]
 #goods
 class Goods(db.Model):
     """
     Class of goods to trade
     """
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64))
-    #picture = app.image_attachment('Image')
+    names = db.Column(db.String(64))
+    picture = app.image_attachment('Image')
     __tablename__="goods"
+class GoodsPicture(db.Model, app.Image):
+    """Goods picture model."""
+    good_id = db.Column(db.Integer, db.ForeignKey('goods.id'), primary_key=True)
+    good_picture = app.relationship('Goods')
+    __tablename__ = 'good_picture'
