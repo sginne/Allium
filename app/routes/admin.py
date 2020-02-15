ba17977d9d42645faf36d2e35233f3e4c3eed6c9
sginne <sigin@norsula.com>
2020-02-15 11:24:29 +0200

    textareafield

--- app/routes/admin.py
+++ app/routes/admin.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, session, request, make_response,abort,redirect
 from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField,PasswordField
+from wtforms import TextAreaField,StringField,SubmitField,PasswordField,TextField
 from flask import current_app as app
 import hashlib
 
@@ -19,19 +19,24 @@ def admin():
     """
     route for admin dashboard
     """
+
     if request.cookies.get('masterkey') == password_hashing(app.config['MASTER_PASSWORD']):
+        #authorized
         return render_template(app.config['TEMPLATE_NAME']+'/admin.html',config=app.config)
     else:
         # fixme add sessions for security, maybe
+        #not authorized, may by form posted
         class AdminForm(FlaskForm):
             master_key=PasswordField('Master password:')
             submit = SubmitField('Log in')
         validation_form=AdminForm()
         if request.method=='POST':
+            #form posted?
             response = make_response(redirect('/admin'))
             response.set_cookie('masterkey', password_hashing(validation_form.master_key.data))
             return response
         else:
+            #return login form
             return render_template(app.config['TEMPLATE_NAME']+'/admin-login.html',form=validation_form)
 
 
@@ -40,7 +45,16 @@ def add_item():
     """
     route for adding item to database
     """
-    abort(400)
+    if request.cookies.get('masterkey') == password_hashing(app.config['MASTER_PASSWORD']):
+        # authorized
+        class AddForm(FlaskForm):
+            name=StringField('Item name:')
+            description=TextAreaField('Description:')
+            submit = SubmitField('Add')
+        add_form=AddForm()
+        return render_template(app.config['TEMPLATE_NAME'] + '/admin-add.html', config=app.config,active="admin-add",form=add_form)
+    else:
+        return redirect('/admin')
 
 @admin_blueprint.route('/logout')
 def logout():
