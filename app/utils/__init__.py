fb00db25b0139979921c9132e2d6877a2b95469d
sginne <sigin@norsula.com>
2020-02-17 08:31:35 +0200

    strong to i, introduced b, regex for img

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -6,11 +6,16 @@ def markup(string_to_parse):
     :param string_to_parse: String to parse into HTML
     :return: returns html
     """
-    #bold_re=re.compile(r"(?<=(?<!\\)(?:\\{2})*)\*\*[^\\*]*(?:\\.[^\\*]*)*\*\*")
-    bold_re=re.compile(r".*?(\*\*(.*?)\*\*).*?")
+    bold_re=re.compile(r".*?(\*\*(.*?)\*\*).*?") # **bold**
+    italic_re=re.compile(r".*?(''(.*?)'').*?")  # ''italics**
+    image_re=re.compile(r".*?(![-(.*?)-]).*?")  # ![-image_id-]
+
+    found_matches=re.findall(italic_re,string_to_parse)
+    for match in found_matches:
+        string_to_parse=string_to_parse.replace(match[0],"<i>"+match[1]+"</i>")
     found_matches=re.findall(bold_re,string_to_parse)
     for match in found_matches:
-        string_to_parse=string_to_parse.replace(match[0],"<strong>"+match[1]+"</strong>")
+        string_to_parse=string_to_parse.replace(match[0],"<b>"+match[1]+"</b>")
     return string_to_parse.replace("\n","<br>")
 def markup_list_descriptions(items_list):
     return_items=[]
