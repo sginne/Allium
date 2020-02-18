f7ccd69f50b0aec049d6391f8f0ed1361d924307
sginne <sigin@norsula.com>
2020-02-19 00:17:45 +0200

    password hashing for all routes

---
+++ app/routes/utils.py
@@ -0,0 +1,9 @@
+import hashlib
+from flask import current_app
+def password_hashing(password):
+    """
+    Returns sha512 from password and app.config['SECRET_KEY']
+    :param password: password string
+    :return: salted and hashed password
+    """
+    return hashlib.pbkdf2_hmac('sha512',password.encode('utf-8'),current_app.config['SECRET_KEY'].encode('utf-8'),66766).hex()#fix me 66766 is for no reason
