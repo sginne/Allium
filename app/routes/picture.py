530d9318ae1a7e47a457eec7e0108cf36f92ebad
sginne <sigin@norsula.com>
2020-02-23 11:00:03 +0200

    basic serve Allium/images/0.jpg

--- app/routes/picture.py
+++ app/routes/picture.py
@@ -1,8 +1,9 @@
-from flask import Response,Blueprint, render_template, session, request, make_response,abort,redirect
+from flask import send_file,Response,Blueprint, render_template, session, request, make_response,abort,redirect
 from flask_wtf import FlaskForm
 from wtforms import TextAreaField,StringField,SubmitField,PasswordField,TextField,HiddenField
 from flask import current_app as app
 from flask import current_app
+import os,io
 
 from app import db,models
 
@@ -13,8 +14,12 @@ picture_blueprint = Blueprint('picture', __name__) #registering picture blueprin
 @picture_blueprint.route('/picture/<pic_id>')
 
 def picture(pic_id):
-
-    try:
-        abort(403)
-    except:
-        abort(404)
+    template_dir = os.path.dirname(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+    template_dir=template_dir+"/images/"
+    print(template_dir)
+    with open(template_dir+"0.jpg", 'rb') as bites:
+        return send_file(
+                     io.BytesIO(bites.read()),
+                     attachment_filename='logo.jpeg',
+                     mimetype='image/jpg'
+               )
