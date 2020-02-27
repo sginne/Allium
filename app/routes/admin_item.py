f8215a6b7f316f26fcf574f9bb3f9b9c9f919191
sginne <sigin@norsula.com>
2020-02-27 21:06:32 +0200

    related currencies query join and relevance in admin-modify-item

--- app/routes/admin_item.py
+++ app/routes/admin_item.py
@@ -5,6 +5,7 @@ from flask import current_app as app
 from flask import current_app
 from app import db,models
 from .. import utils
+
 admin_item_blueprint = Blueprint('admin_item', __name__)
 
 
@@ -42,7 +43,11 @@ def modify_item():
 
     if request.cookies.get('masterkey') == utils.password_hashing(app.config['MASTER_PASSWORD']):
         # authorized
-        items = models.Item.query.all()
+
+        items = db.session.query(models.Item).join(models.Fiat_currency).join(models.Crypto_currency).filter(models.Item.price_crypto_id==models.Crypto_currency.id).filter(models.Item.price_fiat_id==models.Fiat_currency.id).all()
+
+
+
         items=utils.markup_list_descriptions(items)
 
         return render_template(app.config['TEMPLATE_NAME'] + '/admin-modify.html', config=app.config, active="modify-good",items=items)
