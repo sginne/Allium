fda5e23959edd1549894f73268af3f1ad4a7cb78
sginne <sigin@norsula.com>
2020-02-27 15:18:13 +0200

    ? useless

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -6,6 +6,7 @@ from . import populate
 all_tables=["items","pictures","picture_containers","fiat_currency"]
 class Fiat_currency(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
     name=db.Column(db.String(10))
     sign=db.Column(db.String(2))
     __tablename__ = "fiat_currency"
@@ -18,7 +19,8 @@ class Item(db.Model):
     """
     id = db.Column(db.Integer, primary_key=True,autoincrement=True)
     name = db.Column(db.String(64),nullable=False)
-    price_fiat = db.Column(db.Numeric)
+    price_fiat_id=db.Column(db.Integer,db.ForeignKey('fiat_currency.id'))
+    price_fiat=db.relationship("Fiat_currency")
     price_crypto=db.Column(db.Numeric)
     fiat_crypto_main_flag=db.Column(db.String(1))
     description = db.Column(db.String(4096))
