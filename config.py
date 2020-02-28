5c65f11075570a10c90fec1bf7a150d9018afe0e
sginne <sigin@norsula.com>
2020-02-28 09:42:28 +0200

    config.py urwid

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ def read_config(name='allium.cfg'):
                 continue
             if not line.strip():
                 continue
-
+            #print(line)
             key, val = line.strip().split('=')
             #val=val[:-1]
             #print(val)
@@ -15,12 +15,40 @@ def read_config(name='allium.cfg'):
                 val=val[1:-1]
                 conf[key] = val
 def write_config(key,name='allium.cfg'):
-    import fileinput
-    #print('here')
-    for line in fileinput.input(name,inplace=True,backup='.bkp'):
-        print (line)
+
+    with open(name,"r") as config_file:
+        config=config_file.readlines()
+    new_config=[]
+    for line in config:
         if key in line:
-            print (line)
+            import re
+            line=re.sub(r'".*"','"'+conf[key]+'"',line)
+        new_config.append(line)
+    #print(new_config)
+    with open(name,"w") as config_file:
+        config_file.writelines(new_config)
+def change_config(key,val,name='allium.cfg'):
+    conf[key]=val
+    write_config(key)
 read_config()
+#change_config('ALLIUM_TITLE','ZumShop')
 #print(conf['ALLIUM_TITLE'])
-write_config('ALLIUM_TITLE')
+#write_config('ALLIUM_TITLE')
+import urwid
+
+palette = [('palette', 'default,bold', 'default', 'bold'),]
+allium_title_ask = urwid.Edit(('palette', u"ALLIUM_TITLE\n"),edit_text=conf['ALLIUM_TITLE'])
+exit_button = urwid.Button(u'Exit')
+div = urwid.Divider()
+pile = urwid.Pile([allium_title_ask, div, exit_button])
+top = urwid.Filler(pile, valign='top')
+
+
+def on_exit_clicked(button):
+    raise urwid.ExitMainLoop()
+
+
+#urwid.connect_signal(allium_title_ask, 'change', on_ask_change)
+urwid.connect_signal(exit_button, 'click', on_exit_clicked)
+
+urwid.MainLoop(top, palette).run()
