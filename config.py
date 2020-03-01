d74fbc3a8440b63ac7279d6d9ad9bb73c14aa8cc
sginne <sigin@norsula.com>
2020-03-01 06:57:37 +0200

    config.py urwid

--- config.py
+++ config.py
@@ -6,12 +6,8 @@ def read_config(name='allium.cfg'):
                 continue
             if not line.strip():
                 continue
-            #print(line)
             key, val = line.strip().split('=')
-            #val=val[:-1]
-            #print(val)
             if val[0]=="\"" and val[-1:]=="\"":
-                #print(val)
                 val=val[1:-1]
                 conf[key] = val
 def write_config(key,name='allium.cfg'):
@@ -24,31 +20,51 @@ def write_config(key,name='allium.cfg'):
             import re
             line=re.sub(r'".*"','"'+conf[key]+'"',line)
         new_config.append(line)
-    #print(new_config)
     with open(name,"w") as config_file:
         config_file.writelines(new_config)
 def change_config(key,val,name='allium.cfg'):
     conf[key]=val
     write_config(key)
-read_config()
-#change_config('ALLIUM_TITLE','ZumShop')
-#print(conf['ALLIUM_TITLE'])
-#write_config('ALLIUM_TITLE')
-import urwid
 
+read_config()
+import urwid
 palette = [('palette', 'default,bold', 'default', 'bold'),]
-allium_title_ask = urwid.Edit(('palette', u"ALLIUM_TITLE\n"),edit_text=conf['ALLIUM_TITLE'])
-exit_button = urwid.Button(u'Exit')
 div = urwid.Divider()
-pile = urwid.Pile([allium_title_ask, div, exit_button])
+
+allium_title_ask = urwid.Edit(('palette', u"ALLIUM_TITLE: Short and precise, like MyShop\n"),edit_text=conf['ALLIUM_TITLE'])
+allium_name_ask = urwid.Edit(('palette', u"ALLIUM_NAME: Addition to title, like \"Next day delivery!\"\n"),edit_text=conf['ALLIUM_NAME'])
+allium_master_password_ask = urwid.Edit(('palette', u"MASTER_PASSWORD: VERY, EXTREMELY LONG STRING - No-one should be able to guess it. "),edit_text=conf['MASTER_PASSWORD'])
+
+exit_button = urwid.Button(u'Exit')
+cancel_button = urwid.Button(u'Cancel')
+revert_button = urwid.Button(u'Revert')
+
+pile = urwid.Pile([allium_title_ask,allium_name_ask,allium_master_password_ask, div, exit_button,cancel_button])
 top = urwid.Filler(pile, valign='top')
 
 
 def on_exit_clicked(button):
+    #change_config('ALLIUM_TITLE',allium_title_ask.get_text())
+    change_config('ALLIUM_TITLE',conf['ALLIUM_TITLE'])
+    change_config('ALLIUM_NAME',conf['ALLIUM_NAME'])
+    change_config('MASTER_PASSWORD',conf['MASTER_PASSWORD'])
     raise urwid.ExitMainLoop()
-
-
-#urwid.connect_signal(allium_title_ask, 'change', on_ask_change)
+def on_cancel_clicked(button):
+    raise urwid.ExitMainLoop()
+def on_revert_clicked(button):
+    read_config()
 urwid.connect_signal(exit_button, 'click', on_exit_clicked)
+urwid.connect_signal(cancel_button, 'click', on_cancel_clicked)
+urwid.connect_signal(revert_button, 'click', on_revert_clicked)
+
+def on_title_change(edit,new_text):
+    conf['ALLIUM_TITLE']=new_text
+def on_name_change(edit,new_text):
+    conf['ALLIUM_NAME']=new_text
+def on_master_password_change(edit,new_text):
+    conf['MASTER_PASSWORD']=new_text
+urwid.connect_signal(allium_title_ask, 'change', on_title_change)
+urwid.connect_signal(allium_name_ask,'change',on_name_change)
+urwid.connect_signal(allium_master_password_ask,'change',on_master_password_change)
 
 urwid.MainLoop(top, palette).run()
