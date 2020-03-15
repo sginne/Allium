6adcc6f18d9af37cedc2d637eecd5cbcf2406646
sginne <sigin@norsula.com>
2020-03-15 13:40:51 +0200

    work on

---
+++ app/routes/base.py
@@ -0,0 +1,15 @@
+from flask import Response,Blueprint, render_template, session, request, make_response,abort,redirect
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField,StringField,SubmitField,PasswordField,TextField,HiddenField
+from flask import current_app as app
+from flask import current_app
+from .. import currency
+
+from app import db,models
+
+base_blueprint = Blueprint('base', __name__) #registering admin blueprints
+
+@base_blueprint.route('/',methods=['POST','GET'])
+def base():
+    print (currency)
+    return 'lol'
