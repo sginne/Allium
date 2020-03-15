6e6f4bde1bc9b65c091852b0c674b14a87d3e4bb
sginne <sigin@norsula.com>
2020-03-15 13:05:26 +0200

    consolidated FiatCrypto object, exchange rate

--- app/currency/eur_btc.py
+++ app/currency/eur_btc.py
@@ -1,11 +1,12 @@
 import json,urllib
-class EurBtc:
+class FiatCrypto:
 
     exchange_url='https://blockchain.info/ticker'
+    exchange_rate=0
     def __init__(self):
-
+        self.read_exchange_rate()
     def read_exchange_rate(self):
         with urllib.request.urlopen(self.exchange_url) as url:
             data = json.loads(url.read().decode())
-            print(data)
+            self.exchange_rate=data['EUR']['last']
 
