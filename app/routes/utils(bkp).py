8376886e277a1d503cfa22caf9f189e077407993
sginne <sigin@norsula.com>
2020-03-16 01:05:41 +0200

    minor fix

--- app/routes/utils(bkp).py
+++
@@ -1,9 +0,0 @@
-import hashlib
-from flask import current_app
-def password_hashing_deprecate(password):
-    """
-    Returns sha512 from password and app.config['SECRET_KEY']
-    :param password: password string
-    :return: salted and hashed password
-    """
-    return hashlib.pbkdf2_hmac('sha512',password.encode('utf-8'),current_app.config['SECRET_KEY'].encode('utf-8'),66766).hex()#fix me 66766 is for no reason
