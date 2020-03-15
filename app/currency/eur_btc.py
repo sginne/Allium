b2e19922df1d8d7baaf970a7284c65dfaff97381
sginne <sigin@norsula.com>
2020-03-15 23:08:25 +0200

    utils and workhorse added

--- app/currency/eur_btc.py
+++ app/currency/eur_btc.py
@@ -1,12 +1,16 @@
 import json,urllib
+from .. import utils
+#a=utils.ZZ
 class FiatCrypto:
-
+    http=utils.http_module.work_horse()
     exchange_url='https://blockchain.info/ticker'
     exchange_rate=0
     def __init__(self):
         self.read_exchange_rate()
     def read_exchange_rate(self):
-        with urllib.request.urlopen(self.exchange_url) as url:
-            data = json.loads(url.read().decode())
-            self.exchange_rate=data['EUR']['last']
+        data=self.http.json_loads(self.http.url_request(self.exchange_url))
+        self.exchange_rate = data['EUR']['last']
+        #with urllib.request.urlopen(self.exchange_url) as url:
+        #    data = json.loads(url.read().decode())
+        #    self.exchange_rate=data['EUR']['last']
 
