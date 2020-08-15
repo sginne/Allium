b5fdaa1a682a24230c36335d494229d29cc03057
sginne <sigin@norsula.com>
2020-08-15 20:00:05 +0300

    run options

--- run.py
+++ run.py
@@ -4,8 +4,24 @@ if __name__=="__main__":
         for line in config_file:
             if line.find('SSL_ENABLED') != -1:
                 ssl_enabled = line.split('"')[1::2][0]
+            if line.find('HOST') != -1:
+                host = line.split('"')[1::2][0]
 
-    print (ssl_enabled)
-    app.run(port=8080)
+            if line.find('PORT') != -1:
+                port = line.split('"')[1::2][0]
+
+    if ssl_enabled == 'True':
+        import ssl
+
+        context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+        context.load_cert_chain('ssl/web.crt', 'ssl/web.key')
+        app.run(port=port, host=host, ssl_context=context)
+
+    elif ssl_enabled=='adhoc':
+        print("adhoc")
+        context='adhoc'
+        app.run(port=port,host=host,ssl_context=context)
+    else:
+        app.run(port=port, host=host)
 else:
     exit
