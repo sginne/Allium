960dd6524bb5ed087a0b4b50a766e643aee42607
sginne <sigin@norsula.com>
2020-08-16 08:47:44 +0300

    debug change

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from app import app
+
 if __name__=="__main__":
     with open("allium.cfg") as config_file:
         for line in config_file:
@@ -10,12 +11,15 @@ if __name__=="__main__":
             if line.find('PORT') != -1:
                 port = line.split('"')[1::2][0]
 
-    if ssl_enabled == 'True':
+    if ((ssl_enabled == 'True') or (ssl_enabled=='adhoc')):
         import ssl
-
-        context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-        context.load_cert_chain('ssl/web.crt', 'ssl/web.key')
-        app.run(port=port, host=host, ssl_context=context)
+        if ssl_enabled==True:
+            context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+            context.load_cert_chain('ssl/web.crt', 'ssl/web.key')
+            app.run(port=port, host=host, ssl_context=context)
+        else:
+            context = 'adhoc'
+            app.run(port=port, host=host, ssl_context=context)
 
     elif ssl_enabled=='adhoc':
         print("adhoc")
