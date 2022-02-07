c9a7756632bc12ab4ac3fe79e12a512c6de7c553
Timo Junolainen <sigin@norsula.com>
2022-02-07 09:28:23 +0200

    divided post and get for order

--- app/routes/order.py
+++ app/routes/order.py
@@ -13,29 +13,33 @@ class OrderForm(FlaskForm):
     submit = SubmitField('Submit')
 
 order_blueprint = Blueprint('order', __name__) 
-@order_blueprint.route('/order/<item_id>/<amount>/',methods=['GET'])
+@order_blueprint.route('/order/<item_id>/<amount>/',methods=['GET','POST'])
 def order(item_id,amount):
-	try:
-		item_id=str(int(item_id))
-		amount=int(amount)
-	except:
-		item_id="0"
-		amount=0
-	finally:
-		pass
-	order_form=OrderForm()
-	item = db.session.query(models.Item).filter_by(id=item_id).all()
-	item=utils.markup_list_descriptions(item)
-	rate=app.currency_module.exchange_rate
-	pictures = models.Picture.query.all()
-	item = db.session.query(models.Item).filter_by(id=item_id).all()
-	print (item[0].amount_palette)
-	#private_key=app.currency_module.generate_private_key()
-	#crypto_address=app.currency_module.address_from_key(private_key)
-	#wif=app.currency_module.private_to_wif(private_key.hex())
-	#print ("private key: ",private_key.hex())
-	#print ("add: ",crypto_address)
-	#print ("WIF: ",wif)
+    try:
+        item_id=str(int(item_id))
+        amount=int(amount)
+    except:
+        item_id="0"
+        amount=0
+    finally:
+        pass
+    if request.method=='GET':
+        order_form=OrderForm()
+        item = db.session.query(models.Item).filter_by(id=item_id).all()
+        item=utils.markup_list_descriptions(item)
+        rate=app.currency_module.exchange_rate
+        pictures = models.Picture.query.all()
+        item = db.session.query(models.Item).filter_by(id=item_id).all()
+        print ("here",item[0].amount_palette)
+        #private_key=app.currency_module.generate_private_key()
+        #crypto_address=app.currency_module.address_from_key(private_key)
+        #wif=app.currency_module.private_to_wif(private_key.hex())
+        #print ("private key: ",private_key.hex())
+        #print ("add: ",crypto_address)
+		#print ("WIF: ",wif)
+		
 
 
-	return render_template(current_app.config['TEMPLATE_NAME']+'/order.html',form=order_form,amount=amount,rate=rate,fiat_name=app.currency_module.fiat_name,crypto_name=app.currency_module.crypto_name,items=item,pictures=pictures)
+        return render_template(current_app.config['TEMPLATE_NAME']+'/order.html',form=order_form,amount=amount,rate=rate,fiat_name=app.currency_module.fiat_name,crypto_name=app.currency_module.crypto_name,items=item,pictures=pictures)
+    elif request.method=='POST':
+	    return 'post'
