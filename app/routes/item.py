5bf56bea34127da998ea2b686620ddbe2e382edb
Timo Junolainen <sigin@norsula.com>
2022-02-16 06:39:49 +0200

    amount_palette fixed, was splitted by each character, splitted by space now

--- app/routes/item.py
+++ app/routes/item.py
@@ -13,16 +13,20 @@ item_blueprint = Blueprint('item', __name__) #registering admin blueprints
 
 @item_blueprint.route('/item/<item_id>/',methods=['GET'])
 def item(item_id):
-	try:
-		item_id=str(int(item_id))
-	except:
-		item_id="0"
-	finally:
-		pass
-	item = db.session.query(models.Item).filter_by(id=item_id).all()
-	item=utils.markup_list_descriptions(item)
-	rate=app.currency_module.exchange_rate
-	pictures = models.Picture.query.all()
+    try:
+        item_id=str(int(item_id))
+    except:
+        abort(404)
+    finally:
+        pass
+    item = db.session.query(models.Item).filter_by(id=item_id).all()
+    item=utils.markup_list_descriptions(item)
+    rate=app.currency_module.exchange_rate
+    pictures = models.Picture.query.all()
+    try:
+        item[0].amount_palette=item[0].amount_palette.split(" ")
+    except:abort(404)
+    finally: pass
 
-	print(item)
-	return render_template(current_app.config['TEMPLATE_NAME']+'/item.html',rate=rate,fiat_name=app.currency_module.fiat_name,crypto_name=app.currency_module.crypto_name,items=item,pictures=pictures)
+    #print(item)
+    return render_template(current_app.config['TEMPLATE_NAME']+'/item.html',rate=rate,fiat_name=app.currency_module.fiat_name,crypto_name=app.currency_module.crypto_name,items=item,pictures=pictures)
